1413c5c6466895a7b1351d4d9e0a52e483f8c913
234986 <234986@student.pwr.edu.pl>
2019-05-04 17:14:06 +0200

    Add main page footer and button linking to contact form page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {  BrowserRouter, Link, Route } from 'react-router-dom';
-import './../styles/App.css';
 
 import MainPage from './client/mainPage'
 import AdminPage from './admin/adminPage'
 import OfferDetailPage from './client/OfferDetailPage'
+import ContactFormPage from './client/ContactFormPage'
 
 class App extends Component {
   render() {
@@ -15,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component ={MainPage}/>
           <Route exact path="/admin" component ={AdminPage}/>
           <Route exact path="/offerDetail/:id" component = {OfferDetailPage}/>
+          <Route exact path="/contactForm" component = {ContactFormPage}/>
         </BrowserRouter>
       </div>
     </div>
---
+++ src/components/client/ContactFormPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Toaster, Intent } from '@blueprintjs/core'
+import { app} from '../../utilities/base'
+import './../../styles/ContactForm.css';
+
+class ContactFormPage extends Component {
+
+    render() {
+      return (
+        <div>
+          This is a Contact Form Page.
+        </div>
+      );
+    }
+  
+  }
+  export default ContactFormPage;
